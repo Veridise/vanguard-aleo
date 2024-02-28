4c8759562cc729fe0690a5a85955475663300b40
Yanju Chen <chyanju@gmail.com>
2024-02-28 11:39:59 -0800

    added CI for existing detectors

--- tests/scripts/test-infoleak.py
+++ tests/scripts/test-infoleak.py
@@ -1,11 +1,11 @@
 from vanguard.aleo.grammar import AleoEnvironment
-from vanguard.aleo.detectors.divz import detector_divz
+from vanguard.aleo.detectors.infoleak import detector_infoleak
 
 if __name__ == "__main__":
-    project_name = "divz0"
+    project_name = "infoleak0"
     build_path = f"./tests/public/{project_name}/build/"
     env = AleoEnvironment(build_path)
     main = env.main
     fid = "ex0"
-    res, info = detector_divz(env, main.id, fid)
+    res, info = detector_infoleak(env, main.id, fid)
     assert res, f"Test failed for {fid}, expected: True, got: {res}"
